cb88e31ebb2f7accf06654eb537eeeb1ccb8b838
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2021-10-05 08:36:36 +0800

    Don't use struct populate for fuzz test (#9)
    Co-authored-by: Everest Munro-Zeisberger <evmunro1@gmail.com>

--- testutil/network/network_fuzz.go
+++ testutil/network/network_fuzz.go
@@ -3,10 +3,14 @@ package network
 import (
 	"context"
 	"fmt"
+	"math/big"
 	"time"
 
 	"git.fuzzbuzz.io/fuzz"
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/tharsis/ethermint/ethereum/rpc/types"
 )
 
 func FuzzNetworkRawRPC(f *fuzz.F) {
@@ -34,41 +38,38 @@ func FuzzNetworkRawRPC(f *fuzz.F) {
 	}
 }
 
-// func FuzzNetworkBackend(f *fuzz.F) {
-// 	args := types.SendTxArgs{}
-// 	fromAddr := ethcommon.BytesToAddress(f.Bytes("To").Min(20).Max(20).Key())
-// 	gasPriceI := f.Int64("GasPrice").Key()
-// 	gasPrice := big.NewInt(gasPriceI)
-// 	valueI := f.Int64("Value").Key()
-// 	value := big.NewInt(valueI)
-// 	chainI := f.Int64("ChainID").Key()
-// 	chain := big.NewInt(chainI)
-// 	f.Struct("ags").Register(types.SendTxArgs{
-// 		From:       ethcommon.BytesToAddress(f.Bytes("From").Min(20).Max(20).Key()),
-// 		To:         &fromAddr,
-// 		Gas:        (*hexutil.Uint64)(f.Uint64Ptr("Gas").Key()),
-// 		GasPrice:   (*hexutil.Big)(gasPrice),
-// 		Value:      (*hexutil.Big)(value),
-// 		Nonce:      (*hexutil.Uint64)(f.Uint64Ptr("Nonce").Key()),
-// 		Data:       (*hexutil.Bytes)(f.BytesPtr("Data").Key()),
-// 		Input:      (*hexutil.Bytes)(f.BytesPtr("Input").Key()),
-// 		AccessList: &ethtypes.AccessList{},
-// 		ChainID:    (*hexutil.Big)(chain),
-// 	}).Populate(args)
+func FuzzNetworkBackend(f *fuzz.F) {
+	al := &ethtypes.AccessList{}
+	to := ethcommon.BytesToAddress(f.Bytes("To").Min(20).Max(20).Get())
+	args := types.SendTxArgs{
+		From:       ethcommon.BytesToAddress(f.Bytes("From").Min(20).Max(20).Get()),
+		To:         &to,
+		Gas:        (*hexutil.Uint64)(f.Uint64Ptr("Gas").Get()),
+		GasPrice:   (*hexutil.Big)(big.NewInt(f.Int64("GasPrice").Get())),
+		Value:      (*hexutil.Big)(big.NewInt(f.Int64("Value").Get())),
+		Nonce:      (*hexutil.Uint64)(f.Uint64Ptr("Nonce").Get()),
+		Data:       (*hexutil.Bytes)(f.BytesPtr("Data").Get()),
+		Input:      (*hexutil.Bytes)(f.BytesPtr("Input").Get()),
+		ChainID:    (*hexutil.Big)(big.NewInt(f.Int64("ChainID").Get())),
+		AccessList: al,
+	}
+	if !f.GotUpstream() {
+		return
+	}
 
-// 	testnetwork := New(nil, DefaultConfig())
-// 	// nolint
-// 	testnetwork.Validators[0].ETHbackend.SendTransaction(args)
-// 	h, err := testnetwork.WaitForHeightWithTimeout(10, time.Minute)
-// 	if err != nil {
-// 		f.Fail(fmt.Sprintf("expected to reach 10 blocks; got %d", h))
-// 	}
-// 	latestHeight, err := testnetwork.LatestHeight()
-// 	if err != nil {
-// 		f.Fail("latest height failed")
-// 	}
-// 	if latestHeight < h {
-// 		f.Fail("latestHeight should be greater or equal to")
-// 	}
-// 	testnetwork.Cleanup()
-// }
+	testnetwork := New(nil, DefaultConfig())
+	// nolint
+	testnetwork.Validators[0].ETHbackend.SendTransaction(args)
+	h, err := testnetwork.WaitForHeightWithTimeout(10, time.Minute)
+	if err != nil {
+		f.Fail(fmt.Sprintf("expected to reach 10 blocks; got %d", h))
+	}
+	latestHeight, err := testnetwork.LatestHeight()
+	if err != nil {
+		f.Fail("latest height failed")
+	}
+	if latestHeight < h {
+		f.Fail("latestHeight should be greater or equal to")
+	}
+	testnetwork.Cleanup()
+}
