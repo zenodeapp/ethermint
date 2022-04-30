36b9c095b35131c41b5c5f72a2be555e90e0ce11
yihuang <huang@crypto.com>
2022-05-01 00:11:28 +0800

    fix!: london hardfork check logic in json-rpc apis (#1068)

--- x/evm/keeper/keeper_test.go
+++ x/evm/keeper/keeper_test.go
@@ -406,7 +406,7 @@ func (suite *KeeperTestSuite) TestBaseFee() {
 			suite.app.EvmKeeper.BeginBlock(suite.ctx, abci.RequestBeginBlock{})
 			params := suite.app.EvmKeeper.GetParams(suite.ctx)
 			ethCfg := params.ChainConfig.EthereumConfig(suite.app.EvmKeeper.ChainID())
-			baseFee := suite.app.EvmKeeper.BaseFee(suite.ctx, ethCfg)
+			baseFee := suite.app.EvmKeeper.GetBaseFee(suite.ctx, ethCfg)
 			suite.Require().Equal(tc.expectBaseFee, baseFee)
 		})
 	}
